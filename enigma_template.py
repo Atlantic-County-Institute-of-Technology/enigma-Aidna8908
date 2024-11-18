163f07f535f0b8f70f3dcf4ac638e0a031504514
Aidna8908 <117658827+Aidna8908@users.noreply.github.com>
2024-11-18 10:49:32 -0500

    11.18.24
    added code for basic encode message and file
    the mesage is workingthe file is not
    i need to do he decode stuff and fix the file encode

--- enigma_template.py
+++ enigma_template.py
@@ -1,30 +1,76 @@
 # enigma.py
 # description: a simple rotational ciphertext program that can create
 # custom encoded messages, as well as encode and decode from file.
-# author: YOUR_NAME_HERE
-# created: MM.DD.YYYY
-# last update:  MM.DD.YYYY
+# author: aidan
+# created: 11.18.24
+# last update:  11.18.24
 import random
 
 # we'll be using this string for the majority of our translations
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+
 # user inputs a message and selects a key (or random), the message is then translated using the cipher
 def encode_message():
+    result = ""
+    message = input("what is your message?").lower()
+    key = input("what is your key?")
+    key = int(key) % 26
+    for x in message:
+        if 'a' <= x <= 'z':
+            if ord(x) + key >= 123:
+                result += chr(ord(x) + int(x) - 26)
+            elif ord(x) + key <= 96:
+                result += chr(ord(x) + key + 26)
+            else:
+                result += chr(ord(x) + key)
+    print(result)
     pass
+
 
 # encodes a target file, similarly to encode_message, except now targeting a filename
 def encode_file():
+    result = ""
+    x = input("what file")
+    # Read the message from a file
+    with open(x + '.txt', 'r') as file:
+        message = file.read().lower()
+
+    key = input("What is your key? ")
+    key = int(key) % 26
+
+    for x in message:
+        if 'a' <= x <= 'z':
+            if ord(x) + key >= 123:
+                result += chr(ord(x) + key - 26)
+            elif ord(x) + key <= 96:
+                result += chr(ord(x) + key + 26)
+            else:
+                result += chr(ord(x) + key)
+    print(f"Please select an option:\n"
+          f"[1]: write into file.\n"
+          f"[2]: print result.")
+    selection = input("Choose an option:")
+
+    if selection == 1:
+        with open('output.txt', 'w') as file:
+            file.write(result)
+
+        print("The result has been written to output.txt")
+    if selection == 2:
+        print(result)
     pass
+
 
 # decodes target file using a user-specified key. If key is unknown, a keypress should
 # call decode_unknown_key()
 def decode_file():
     pass
 
+
 # runs if the key is unknown. If this is true, print out all possible decoding combinations.
 def decode_unknown_key(filename):
-   pass
+    pass
 
 
 # main method declaration
@@ -50,6 +96,7 @@ def main():
             exit()
         else:
             print("Invalid choice. Please try again.")
+
 
 # runs on program start
 if __name__ == "__main__":
